9dad8392da5acee2b190715b1165e48c3055fce3
soyluemre <emresoyludev@gmail.com>
2023-04-24 00:30:03 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,22 @@ import { Container, Row } from "reactstrap";
 import logo from "../../assets/images/eco-logo.png";
 import { NavLink } from "react-router-dom";
 import userIcon from "../../assets/images/user-icon.png";
+import { motion } from "framer-motion";
+
+const nav__links = [
+  {
+    path: "home",
+    display: "Home",
+  },
+  {
+    path: "shop",
+    display: "Shop",
+  },
+  {
+    path: "cart",
+    display: "Cart",
+  },
+];
 
 const Header = () => {
   return (
@@ -15,33 +31,42 @@ const Header = () => {
               <img src={logo} alt="logo" />
               <div>
                 <h1>Multimart</h1>
-                <p>Since 1995</p>
+                {/* <p>Since 1995</p> */}
               </div>
             </div>
 
             <div className="navigation">
               <ul className="menu">
-                <li className="nav__item">
-                  <NavLink to="home">Home</NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink to="shop">Shop</NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink to="cart">Cart</NavLink>
-                </li>
+                {nav__links?.map((item, index) => (
+                  <li key={index} className="nav__item">
+                    <NavLink
+                      className={(navClass) =>
+                        navClass?.isActive ? "nav__active" : ""
+                      }
+                      to={item?.path}
+                    >
+                      {item?.display}
+                    </NavLink>
+                  </li>
+                ))}
               </ul>
             </div>
 
             <div className="nav__icons">
               <span className="fav__icon">
                 <i className="ri-heart-line"></i>
+                <span className="badge">100</span>
               </span>
               <span className="cart__icon">
                 <i className="ri-shopping-bag-line"></i>
+                <span className="badge">100</span>
               </span>
               <span>
-                <img src={userIcon} alt="usericon" />
+                <motion.img
+                  whileTap={{ scale: 1.2 }}
+                  src={userIcon}
+                  alt="usericon"
+                />
               </span>
             </div>
 
