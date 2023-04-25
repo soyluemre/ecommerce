f4a77d2fd9b715c4093100a1b4e4dba4031e7702
soyluemre <emresoyludev@gmail.com>
2023-04-25 15:55:01 +0300

    create Home and Helmet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { motion } from "framer-motion";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img.png";
+import "../styles/home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const year = new Date().getFullYear();
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending Product in {year}</p>
+                <h2>Make Your Interior More Minimalistic & Modern</h2>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam
+                  similique esse, ad quod animi et. Excepturi iusto quas minus
+                  sint.
+                </p>
+                <motion.button
+                  whileTap={{
+                    scale: 1.2,
+                  }}
+                  className="buy__btn"
+                >
+                  <Link to="/shop">SHOP NOW</Link>
+                </motion.button>
+              </div>
+            </Col>
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="heroimage" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
