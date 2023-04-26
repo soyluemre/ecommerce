63a31f549c082f5bdd2f51d663482395af59da70
soyluemre <emresoyludev@gmail.com>
2023-04-27 02:52:52 +0300

    edit css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="footer">
       <Container>
         <Row>
-          <Col lg="4">
+          <Col lg="4" className="mb-4" md="6">
             <div className="logo">
               <div>
                 <h1 className="text-white">Multimart</h1>
@@ -21,7 +21,7 @@ const Footer = () => {
               Reiciendis ea quae nihil.
             </p>
           </Col>
-          <Col lg="3">
+          <Col lg="3" className="mb-4" md="3">
             <div className="footer__quick-links">
               <h4 className="quick__links-title">Top Categories</h4>
               <ListGroup>
@@ -43,7 +43,7 @@ const Footer = () => {
               </ListGroup>
             </div>
           </Col>
-          <Col lg="2">
+          <Col lg="2" className="mb-4" md="3">
             <div className="footer__quick-links">
               <h4 className="quick__links-title">Use Links</h4>
               <ListGroup>
@@ -65,7 +65,7 @@ const Footer = () => {
               </ListGroup>
             </div>
           </Col>
-          <Col lg="3">
+          <Col lg="3" md="4">
             <div className="footer__quick-links">
               <h4 className="quick__links-title">Contact</h4>
               <ListGroup className="footer__contact">
